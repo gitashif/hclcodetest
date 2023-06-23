12b5d4c149d668eb65093b7454aef7dc0307b2a4
gitashif <ashifece@gmail.com>
2023-06-23 19:15:59 +0530

    python webapp

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# hclcodetest
-Repo to work code test
+# Used Streamlit to create a personal website
+
+Create a personal website using Python and the Streamlit library.
+
+## Required libraries
+```
+python==3.11
+streamlit==1.3.0
+
+```
+
+
---
+++ app.py
@@ -0,0 +1,54 @@
+import requests
+import streamlit as st
+from PIL import Image
+
+
+# Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
+st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
+
+
+def load_lottieurl(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+
+# Use local CSS
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+local_css("style/style.css")
+
+# ---- LOAD ASSETS ----
+lottie_coding = load_lottieurl("https://assets5.lottiefiles.com/packages/lf20_fcfjwiyb.json")
+img_contact_form = Image.open("images/yt_contact_form.png")
+img_lottie_animation = Image.open("images/yt_lottie_animation.png")
+
+# ---- HEADER SECTION ----
+with st.container():
+    st.subheader("Hi, I am Ashif:wave:")
+    st.title("A Devops Engineer From HCL")
+    st.write(
+        "Passionate about Devops."
+    )
+    st.write("[Want to know more about me >](https://www.linkedin.com/in/mohamed-ashif-g-88526555/)")
+
+# ---- Code Test completed!! ----
+with st.container():
+    st.write("---")
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.header("What I do")
+        st.write("##")
+        st.write(
+            """
+           - webpage built using python
+           - containerized using docker
+           - Code pushed to github repo
+            """
+        )
+        st.write("[More details>](https://github.com/anz-ecp/ashif_mohamed)")
+
